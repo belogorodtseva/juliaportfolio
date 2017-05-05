1a2eefdacf3798d0cb1350a94d50ce1506c05d49
belogorodtseva <annbelogorodtseva@gmail.com>
2017-05-05 18:05:11 +0300

    create test version

---
+++ surova/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, render_to_response
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.shortcuts import get_list_or_404, get_object_or_404
+
+
+
+def index(request):
+    return render(request, 'surova/home.html')
+
+def projects(request):
+    return render(request, 'surova/projects.html')
